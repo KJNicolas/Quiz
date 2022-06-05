feb515e116872f9d033eeabb2e549e734a5e0bb5
Freedman J Nicolas <100964607+KJNicolas@users.noreply.github.com>
2022-06-05 09:00:41 -0700

    Main

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+    numb: 1,
+    question: "What Country has the highest life expectancy?",
+    answer: "Hong Kong",
+    options: [
+      "USA",
+      "Hong Kong",
+      "Brazil",
+      "Canada"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Where would you be if you were standing on the Spanish Steps?",
+    answer: "Rome",
+    options: [
+      "woods",
+      "Italy",
+      "Rome",
+      "England"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What is the most common surname in the United States? ",
+    answer: "Smith",
+    options: [
+      "John",
+      "Smith",
+      "Jack",
+      "Jane"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What year was the United Nations established? ",
+    answer: "1945",
+    options: [
+      "1945",
+      "1938",
+      "1950",
+      "1944"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Who has won the most total Academy Awards?",
+    answer: "Walt Disney",
+    options: [
+      "Walt Disney",
+      "Marvel Universe",
+      "Warner Bros",
+      "Sony"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What artist has the most streams on Spotify?",
+    answer: "Drake",
+    options: [
+      "Lil Wayne",
+      "Usher",
+      "Drake",
+      "Rick-Ross"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Who discovered that the earth revolves around the sun?",
+    answer: "Nicolaus Copernicus",
+    options: [
+      "Elon Musk",
+      "Albert Einstein",
+      "Nicolaus Copernicus",
+      "Smith Scott"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What Netflix show had the most streaming views in 2021?",
+    answer: "Squid Game",
+    options: [
+      "Squid Game",
+      "Raising Dion",
+      "Black Mirror",
+      "Sex Education"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which grammy-nominated New York rapper died in April of 2021?",
+    answer: "DMX",
+    options: [
+      "DMX",
+      "xxxtentacion",
+      "MacMiller",
+      "Biggie"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is the largest Spanish speaking city in the world?",
+    answer: "Mexico City",
+    options: [
+      "San Antonio",
+      "Mexico City",
+      "Bronx",
+      "Portland"
+    ]
+  },
+
+];
